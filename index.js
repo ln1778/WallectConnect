97e5a1576e360d95c5a6794f89534dc6f4a4e672
ln1778 <465410291@qq.com>
2023-03-14 11:46:55 +0800

    333

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const WalletConnect=require("./walletconnect");
 const ReactQrcode =require("qrcode.react");
 const QrcodeModal =require("./qrcode-modal");
 
-module.exports.client= WalletConnect.default;
-module.exports.Qrcode=ReactQrcode;
+module.exports.Client= WalletConnect.default;
+module.exports.Qrcode=ReactQrcode.default;
 module.exports.QrcodeModal=QrcodeModal;
